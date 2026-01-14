a28a200aae6edb8f0b952d8edf4656ec7b22a5d3
Jeevan Adhikari <jeevanadhikaritech@gmail.com>
2026-01-14 15:10:54 +0545

    Fix: bump API version from 0.0.1 to 0.0.2

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const option = {
     openapi: "3.0.0",
     info: {
       title: "TODO-WORKSPACE API",
-      version: "0.0.1",
+      version: "0.0.2",
       description: "This is a simple API for TODO-WORKSPACE",
     },
     servers: [
